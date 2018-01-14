bbfd10b86d8f1758ef0fce261d1922a6d807b654
AdirakhmatM <32364278+AdirakhmatM@users.noreply.github.com>
2018-01-14 20:15:14 +0800

    Output variable

--- TugasclassAd.cpp
+++ TugasclassAd.cpp
@@ -26,4 +26,20 @@ int main(){
 	sony.kelas ="Mid End - High End - Low End  |\n";
 	iphone.kelas ="High End 			  |\n";
 	
+	cout << " Handphone \n";
+	cout << "+---------------+--------------------------------+----------------------------------------+\n";
+	cout << "| Nokia		" << "| Kualitas : " <<(nokia.kualitas)	<<" | " <<" Kelas : " << (nokia.kelas);
+	cout << "| Xiaomi 	" << "| Kualitas : " <<(xiaomi.kualitas)	<<" | " <<" Kelas : " << (xiaomi.kelas);
+	cout << "| Samsung	" << "| Kualitas : " <<(samsung.kualitas)<<" | " <<" Kelas : " << (samsung.kelas);
+	cout << "| Sony 		" << "| Kualitas : " <<(sony.kualitas)	<<" | " <<" Kelas : " << (sony.kelas);
+	cout << "| Iphone 	" << "| Kualitas : " <<(iphone.kualitas)	<<" | " <<" Kelas : " << (iphone.kelas);
+	cout << "+---------------+--------------------------------+----------------------------------------+\n";
+	
+	cout << " Survey Handphone : \n";
+	cout << " ----------------- \n";
+	nokia.pendapat	(" Nokia 	: Tahan Lama , Spek Menengah , Bintang 4 ");
+	xiaomi.pendapat	(" Xiaomi 	: Lumayan Lama , Spek Tinggi , Bintang 4 ");
+	samsung.pendapat(" Samsung 	: Tahan Lama , Spek Menengah Ke Atas , Bintang 4.5 ");
+	sony.pendapat	(" Sony 	: Lumayan Lama , Spek Menengah , Bintang 4 ");
+	iphone.pendapat	(" Iphone 	: Tahan Lama , Spek Tinggi , Bintang 4.7 ");
 } 
