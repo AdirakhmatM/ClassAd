a27b0b679cdcb458ee00021920565963bdadc846
AdirakhmatM <32364278+AdirakhmatM@users.noreply.github.com>
2018-01-14 20:14:35 +0800

    Input variable dan nilai variable

--- TugasclassAd.cpp
+++ TugasclassAd.cpp
@@ -12,5 +12,18 @@ class hp{
 };
 
 int main(){
+	hp nokia, xiaomi, samsung, sony, iphone;
+	
+	nokia.kualitas ="Sangat Baik 	";
+	xiaomi.kualitas ="Bagus 		";
+	samsung.kualitas ="Sangat Bagus 	";
+	sony.kualitas ="Baik 		";
+	iphone.kualitas ="Baik dan Bagus 	";
+	
+	nokia.kelas ="Mid End - High End 		  |\n";
+	xiaomi.kelas ="Mid End - Low End 		  |\n";
+	samsung.kelas ="Mid End - High End 		  |\n";
+	sony.kelas ="Mid End - High End - Low End  |\n";
+	iphone.kelas ="High End 			  |\n";
 	
 } 
